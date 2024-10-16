670fb10ad4a2c94cf2903acc5feceef648a292ea
Lawani-EJ <elyonjohn60@gmail.com>
2024-10-16 12:42:26 +0100

    Creating another alive-progress demo Here i loop over four different integer values and create a progress bar for each of them Then loop over a range of one thousand and the progress bar runs through to completion.

--- script.py
+++ script.py
@@ -1,3 +1,8 @@
-from alive_progress.styles import showtime
+from alive_progress import alive_bar
+import time
 
-showtime()
+for x in 1000, 1500, 700, 0:
+    with alive_bar(x) as bar:
+        for i in range(1000):
+            time.sleep(.005)
+            bar()
